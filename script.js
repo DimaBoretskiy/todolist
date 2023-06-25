381cb04b64a8c472493cb98f51e9e199a3ab0b6f
DimaBoretskiy <137459536+DimaBoretskiy@users.noreply.github.com>
2023-06-25 20:01:36 +0300

    Add todolist

---
+++ script.js
@@ -0,0 +1,44 @@
+const todoContainer = document.querySelector("#todoList")
+const button = document.querySelector("#addButton")
+let todoList = []
+const input = document.querySelector('#addInput');
+
+if(localStorage.getItem('todo')){
+   todoList =  JSON.parse(localStorage.getItem('todo'));
+   updateTodo()
+}
+
+button.addEventListener('click',()=>{
+    const newTodo = input.value
+    if(newTodo){
+        todoList.push({status:false,name: newTodo})
+        localStorage.setItem('todo',JSON.stringify(todoList))
+        input.value = ''
+        updateTodo()
+    } else{
+        alert('введите данные')
+    }  
+})
+
+function updateTodo(){
+    todoContainer.innerHTML = '';
+    todoList.forEach((element,index) => {
+        let newInput = element.status ?
+        '<input checked onclick="updateStatus('+index+')" type="checkbox" >' 
+        :'<input onclick="updateStatus('+index+')" type="checkbox" >'
+      
+        todoContainer.innerHTML += '<li>' + newInput + element.name + '<button onclick="deleteTodo('+index+')">Удалить</button></li>';
+    });
+}
+
+function updateStatus(index){
+    todoList[index].status = !todoList[index].status 
+    localStorage.setItem('todo', JSON.stringify(todoList));
+    updateTodo()
+}
+
+function deleteTodo(index){
+    todoList.splice(index, 1);
+    localStorage.setItem('todo', JSON.stringify(todoList));
+    updateTodo()
+}
